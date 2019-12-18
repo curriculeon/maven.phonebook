d8698932c724e168204fb7baf32dce94abe29807
unknown <joulia.kim@gmail.com>
2019-12-18 16:09:26 -0500

    simple Phone book is done

--- src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
+++ src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
@@ -1,5 +1,7 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,20 +9,27 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class AdvancedPhoneBook {
+    Map<String, List<String>> contacts ;
+    ArrayList<String> numberList = new ArrayList<String>();
     public AdvancedPhoneBook(Map<String, List<String>> map) {
+        contacts = map;
     }
 
     public AdvancedPhoneBook() {
-        this(null);
+        contacts = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        numberList.add(phoneNumber);
+        contacts.put(name, numberList);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
     }
 
     public void remove(String name) {
+        contacts.remove(name);
     }
 
     public Boolean hasEntry(String name) {
@@ -32,6 +41,11 @@ public class AdvancedPhoneBook {
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for (Map.Entry<String, List<String>> entry : contacts.entrySet()) {
+            System.out.println(entry.getValue());
+            //if (entry.getValue().contains(phoneNumber))
+               // return entry.getKey();
+        }
         return null;
     }
 
--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,32 +1,39 @@
 package com.github.curriculeon;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
-    public void add(String name, String phoneNumberToAdd) {
+    LinkedHashMap<String, String> contacts = new LinkedHashMap<String, String>();
 
+    public void add(String name, String phoneNumberToAdd) {
+        contacts.put(name, phoneNumberToAdd);
     }
 
     public String get(String name) {
-        return null;
+        return contacts.get(name);
     }
 
     public Boolean hasEntry(String phoneNumber) {
-        return null;
+        return contacts.containsValue(phoneNumber);
     }
 
     public void remove(String name) {
-
+        contacts.remove(name);
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> list =new ArrayList<String>();
+        contacts.forEach((key, value) -> list.add(key));
+        return list;
     }
 
     public String reverseLookup(String phoneNumber) {
+        for (Map.Entry<String, String> entry : contacts.entrySet())
+            if(phoneNumber == entry.getValue())
+                return entry.getKey();
         return null;
     }
 }
