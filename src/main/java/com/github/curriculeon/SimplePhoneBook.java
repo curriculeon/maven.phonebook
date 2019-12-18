86180b94e95ff4690b6ea3425c62dc88f1228f15
Erjon Dervishaj <er_djs@yahoo.com>
2019-12-17 23:33:52 -0500

    first commit

--- src/main/java/com/github/curriculeon/SimplePhoneBook.java
+++ src/main/java/com/github/curriculeon/SimplePhoneBook.java
@@ -1,17 +1,20 @@
 package com.github.curriculeon;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by leon on 12/16/2019.
  */
 public class SimplePhoneBook {
+    private Map<String, String> phoneBook = new HashMap<>();
     public void add(String name, String phoneNumberToAdd) {
 
     }
 
     public String get(String name) {
-        return null;
+        return phoneBook.get(name);
     }
 
     public Boolean hasEntry(String phoneNumber) {
