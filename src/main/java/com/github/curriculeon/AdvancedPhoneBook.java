1683b59f6334e6e8e5f1736b37d704a656247fa5
unknown <joulia.kim@gmail.com>
2019-12-19 15:19:22 -0500

    done

--- src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
+++ src/main/java/com/github/curriculeon/AdvancedPhoneBook.java
@@ -1,18 +1,19 @@
 package com.github.curriculeon;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
+
+
 public class AdvancedPhoneBook {
-    Map<String, List<String>> contacts ;
-    ArrayList<String> numberList = new ArrayList<String>();
+
+    Map<String, List<String>> contacts;
+    List<String> numberlist;
     public AdvancedPhoneBook(Map<String, List<String>> map) {
-        contacts = map;
+        contacts = new LinkedHashMap<>();
+        contacts.putAll(map);
     }
 
     public AdvancedPhoneBook() {
@@ -20,12 +21,20 @@ public class AdvancedPhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
-        numberList.add(phoneNumber);
-        contacts.put(name, numberList);
+        if (contacts.containsKey(name)) {
+            contacts.get(name).add(phoneNumber);
+        } else {
+            numberlist=new ArrayList<String>();
+            numberlist.add(phoneNumber);
+            contacts.put(name,numberlist);
+        }
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
-
+        for (String s : phoneNumbers) {
+            add(name, s);
+        }
     }
 
     public void remove(String name) {
@@ -33,27 +42,27 @@ public class AdvancedPhoneBook {
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return contacts.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return contacts.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        for (Map.Entry<String, List<String>> entry : contacts.entrySet()) {
-            System.out.println(entry.getValue());
-            //if (entry.getValue().contains(phoneNumber))
-               // return entry.getKey();
-        }
+        for (Map.Entry<String, List<String>> entry : contacts.entrySet())
+            if(entry.getValue().contains(phoneNumber))
+                return entry.getKey();
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> list =new ArrayList<String>();
+        contacts.forEach((key, value) -> list.add(key));
+        return list;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return contacts;
     }
 }
